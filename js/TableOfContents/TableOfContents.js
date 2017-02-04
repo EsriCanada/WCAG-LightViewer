ec057e5b9085b39d08d72c0c4d0c3856861b809b
Horia Tudosie <htudosie@esri.ca>
2017-02-03 23:06:10 -0500

    Restyled Label For Layer in TableOfContents

--- js/TableOfContents/TableOfContents.js
+++ js/TableOfContents/TableOfContents.js
@@ -173,10 +173,10 @@ define(["dojo/Evented", "dojo/_base/declare", "dojo/_base/lang", "dojo/has", "es
                         checked: layer.visibility,
                     }, titleContainerDiv);
 
-                    var titleText = domConstruct.create("label", {
-                        for: "layer_ck_"+i,
+                    var titleText = domConstruct.create("div", {
+                        //for: "layer_ck_"+i,
                         className: this.css.titleText,
-                        innerHTML: '<div class="labelText">'+layer.title+'</div>',
+                        innerHTML: '<label for="layer_ck_'+i+'" class="labelText">'+layer.title+'</div>',
                         // role: "presentation",
                         title : layer.title,
                         tabindex:0,
